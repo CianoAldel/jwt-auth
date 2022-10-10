8bbf8f98cfb8f7e2e01a2ff007879a926aba2df9
MSI\Ciano Aldel <khamsing111@hotmail.co.th>
2022-10-10 15:14:25 +0700

    no message

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,6 +1,5 @@
 const defaultConfig = {
-    // env: process.env.NODE_ENV,
-    // port: process.env.PORT || 3000,
+    env: process.env.NODE_ENV,
     port: process.env.PORT || 3000,
     prefix: '/'
 };
--- app/index.js
+++ app/index.js
@@ -1,4 +1,35 @@
-// if (process.env.NODE_ENV === '') require('dotenv').config();
+import express from 'express';
+import routes from './routes';
+import config from './config/constants'
 
-// Initialize Server
-require('./server');
+const path = require('path')
+
+const cors = require("cors");
+
+const app = express();
+app.use(express.json())
+
+
+global.__basedir = __dirname;
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
+var corsOptions = {
+  origin: "http://localhost:3000"
+};
+app.use(cors(corsOptions));
+
+app.use("/images", express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'resources')));
+
+app.use(config.prefix, routes);
+
+
+app.listen(config.port, () => {
+    console.log(`
+    Port: ${config.port}
+    Env: ${app.get('env')}
+  `);
+});
